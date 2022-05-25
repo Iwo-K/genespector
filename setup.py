057571d978ee6cf76eba184a1c94805204548222
idk25 <iwo.kucinski@gmail.com>
2022-05-25 11:32:52 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='genespector',
-    version='0.0.1',
+    version='0.1.0',
     description='Package for interactive viewing of AnnData objects/files',
     license='MIT',
     packages=['genespector'],
