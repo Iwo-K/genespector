2753a321631c1effaf5c1b9a7cc2f9f2fdad0190
idk25 <iwo.kucinski@gmail.com>
2022-05-25 10:51:31 +0100

    Black formatted code for readability

--- genespector/app.py
+++ genespector/app.py
@@ -12,24 +12,27 @@ import scanpy as sc
 from pathlib import Path
 from anndata import AnnData
 
-#Function for running the genespector
+# Function for running the genespector
 
-def make_app(adata = None,
-    use_raw = False,
-    make_var_unique = True,
-    main_title = "Genespector",
-    subtitle = None,
-    layout = None,
-    server = True,
-    url_base_pathname = '/',
-    run = True):
 
-    #Specifying the current path to access the example dataset
+def make_app(
+    adata=None,
+    use_raw=False,
+    make_var_unique=True,
+    main_title="Genespector",
+    subtitle=None,
+    layout=None,
+    server=True,
+    url_base_pathname="/",
+    run=True,
+):
+
+    # Specifying the current path to access the example dataset
     HERE = Path(__file__).parent
 
-    #adata argument checking and processing
+    # adata argument checking and processing
     if adata is None:
-        adata_path = HERE / 'data/tiny_example2.h5ad'
+        adata_path = HERE / "data/tiny_example2.h5ad"
         adata = sc.read(adata_path)
 
     elif isinstance(adata, str):
@@ -39,130 +42,166 @@ def make_app(adata = None,
         pass
 
     else:
-        print('Error: adata argument needs to be either an AnnData object or path ')
+        print("Error: adata argument needs to be either an AnnData object or path ")
         return
 
-    #Importing data
+    # Importing data
     graphdata = import_adata(adata, use_raw, make_var_unique)
-    #Setting up dash server
-    app = dash.Dash(__name__,
-        server = server,
-        url_base_pathname = url_base_pathname)
+    # Setting up dash server
+    app = dash.Dash(__name__, server=server, url_base_pathname=url_base_pathname)
 
-    #App Layout
+    # App Layout
     if subtitle is None:
-        subtitle = 'Data: LK/LSK cells (Dahlin et al. 2018)'
+        subtitle = "Data: LK/LSK cells (Dahlin et al. 2018)"
 
     if layout is None:
         app.layout = make_layout(graphdata, main_title, subtitle)
     else:
         app.layout = layout(graphdata, main_title, subtitle)
 
-    ''' App callback
-    Specifies inputs and outputs from sliders and dropdown menus
-    and generates graph objects, which are updated respectively
-    '''
     @app.callback(
-        dash.dependencies.Output('maingraph', 'figure'),
-        [dash.dependencies.Input('split_dropdown', 'value'),
-        dash.dependencies.Input('color_dropdown', 'value'),
-        dash.dependencies.Input('Xaxis_choice', 'value'),
-        dash.dependencies.Input('Yaxis_choice', 'value'),
-        dash.dependencies.Input('pointsize_slider', 'value'),
-        dash.dependencies.Input('Axis_switch', 'value'),
-         dash.dependencies.Input('cmin_input', 'value'),
-         dash.dependencies.Input('cmax_input', 'value'),
-         dash.dependencies.Input('opacity_slider', 'value')])
-    def update_figure(splitbyvalue, colorvalue, Xaxis, Yaxis, pointsize, axiswitchvalue, cmin_input, cmax_input, opacity):
+        dash.dependencies.Output("maingraph", "figure"),
+        [
+            dash.dependencies.Input("split_dropdown", "value"),
+            dash.dependencies.Input("color_dropdown", "value"),
+            dash.dependencies.Input("Xaxis_choice", "value"),
+            dash.dependencies.Input("Yaxis_choice", "value"),
+            dash.dependencies.Input("pointsize_slider", "value"),
+            dash.dependencies.Input("Axis_switch", "value"),
+            dash.dependencies.Input("cmin_input", "value"),
+            dash.dependencies.Input("cmax_input", "value"),
+            dash.dependencies.Input("opacity_slider", "value"),
+        ],
+    )
+    def update_figure(
+        splitbyvalue,
+        colorvalue,
+        Xaxis,
+        Yaxis,
+        pointsize,
+        axiswitchvalue,
+        cmin_input,
+        cmax_input,
+        opacity,
+    ):
+        """App callback
+        Specifies inputs and outputs from sliders and dropdown menus
+        and generates graph objects, which are updated respectively
+        """
 
-        if colorvalue == 'categorical data':
+        if colorvalue == "categorical data":
             pass
-        elif colorvalue in graphdata['numkeys']:
-            colors = np.squeeze(np.array(graphdata['numinfo'][:,graphdata['numkeys'] == colorvalue]))
+        elif colorvalue in graphdata["numkeys"]:
+            colors = np.squeeze(
+                np.array(graphdata["numinfo"][:, graphdata["numkeys"] == colorvalue])
+            )
         else:
-            colors = np.squeeze(np.array(graphdata['genexpr'][:,graphdata['genekeys'] == colorvalue].todense()))
+            colors = np.squeeze(
+                np.array(
+                    graphdata["genexpr"][
+                        :, graphdata["genekeys"] == colorvalue
+                    ].todense()
+                )
+            )
 
         traces = []
 
-        category = graphdata['catinfo'][splitbyvalue].cat.categories
+        category = graphdata["catinfo"][splitbyvalue].cat.categories
 
         for i in category:
 
-            boolSUB = graphdata['catinfo'][splitbyvalue] == i
+            boolSUB = graphdata["catinfo"][splitbyvalue] == i
 
-            if colorvalue == 'categorical data':
+            if colorvalue == "categorical data":
 
                 graph = go.Scattergl(
-                    x = graphdata['coords'].loc[boolSUB,:][Xaxis].values,
-                    y = graphdata['coords'].loc[boolSUB,:][Yaxis].values,
-                    mode = 'markers',
-                    marker = dict(opacity = opacity/100,
-                        size = pointsize),
-                    name = str(i))
+                    x=graphdata["coords"].loc[boolSUB, :][Xaxis].values,
+                    y=graphdata["coords"].loc[boolSUB, :][Yaxis].values,
+                    mode="markers",
+                    marker=dict(opacity=opacity / 100, size=pointsize),
+                    name=str(i),
+                )
 
-                #Checking if there is matchig color code for the category
-                if splitbyvalue in graphdata['color_catinfo'].keys():
-                    marker_color = graphdata['color_catinfo'][splitbyvalue][i]
-                    graph['marker']['color'] = marker_color
+                # Checking if there is matchig color code for the category
+                if splitbyvalue in graphdata["color_catinfo"].keys():
+                    marker_color = graphdata["color_catinfo"][splitbyvalue][i]
+                    graph["marker"]["color"] = marker_color
 
                 traces.append(graph)
 
             else:
                 colorSUB = colors[boolSUB]
-                catinfoSUB = graphdata['catinfo'].loc[boolSUB,:]
+                catinfoSUB = graphdata["catinfo"].loc[boolSUB, :]
 
-                #Added possibility of having diverging colorscale
+                # Added possibility of having diverging colorscale
                 if any(colors < 0) and any(colors > 0):
-                # colorscale = 'RdBu'
-                    colorscale = [[0, 'rgba(31, 58, 147, 1)'],
-                    [-min(colors)/(max(colors)-min(colors)), '#DCDCDC'],
-                    [1, 'rgba(207, 0, 15, 1)']]
+                    # colorscale = 'RdBu'
+                    colorscale = [
+                        [0, "rgba(31, 58, 147, 1)"],
+                        [-min(colors) / (max(colors) - min(colors)), "#DCDCDC"],
+                        [1, "rgba(207, 0, 15, 1)"],
+                    ]
                 else:
-                    colorscale = 'Viridis'#'Blackbody'
+                    colorscale = "Viridis"  #'Blackbody'
 
                 # Specifying the markers
                 cmin = min(colors)
                 cmax = max(colors)
-                if cmin_input != '':
+                if cmin_input != "":
                     cmin = float(cmin_input)
-                if cmax_input != '':
+                if cmax_input != "":
                     cmax = float(cmax_input)
-                marker = dict(color = colorSUB,
-                    cmin = cmin,
-                    cmax = cmax,
-                    opacity = opacity/100,
-                    colorscale = colorscale,
-                    colorbar = dict(thickness=14, x = 1.15, title = colorvalue, titleside = 'bottom'), #perhaps make optional?, then just pass dict() to switch off
-                    size = pointsize)
+                marker = dict(
+                    color=colorSUB,
+                    cmin=cmin,
+                    cmax=cmax,
+                    opacity=opacity / 100,
+                    colorscale=colorscale,
+                    colorbar=dict(
+                        thickness=14, x=1.15, title=colorvalue, titleside="bottom"
+                    ),  # perhaps make optional?, then just pass dict() to switch off
+                    size=pointsize,
+                )
 
-                traces.append(go.Scattergl(
-                    x = graphdata['coords'].loc[boolSUB,:][Xaxis].values,
-                    y = graphdata['coords'].loc[boolSUB,:][Yaxis].values,
-                    mode = 'markers',
-                    marker = marker,
-                    name = str(i),
-                    text = [catinfoSUB.index.values[x] + '<br>' +
-                     splitbyvalue + ' ' + catinfoSUB[splitbyvalue].values[x] +
-                       '<br>' + str(colorSUB[x]) for x in range(0, len(catinfoSUB.index))],
-                        hoverinfo = 'text')
-                            )
+                traces.append(
+                    go.Scattergl(
+                        x=graphdata["coords"].loc[boolSUB, :][Xaxis].values,
+                        y=graphdata["coords"].loc[boolSUB, :][Yaxis].values,
+                        mode="markers",
+                        marker=marker,
+                        name=str(i),
+                        text=[
+                            catinfoSUB.index.values[x]
+                            + "<br>"
+                            + splitbyvalue
+                            + " "
+                            + catinfoSUB[splitbyvalue].values[x]
+                            + "<br>"
+                            + str(colorSUB[x])
+                            for x in range(0, len(catinfoSUB.index))
+                        ],
+                        hoverinfo="text",
+                    )
+                )
 
-        if axiswitchvalue == 'ON':
-            axiscolor = '#BBBBBB'
-        else: axiscolor = '#ffffff'
+        if axiswitchvalue == "ON":
+            axiscolor = "#BBBBBB"
+        else:
+            axiscolor = "#ffffff"
 
         return {
-            'data': traces,
-            'layout': {
-                'xaxis' : {'title': Xaxis, 'color' : axiscolor},
-                'yaxis' : {'title': Yaxis, 'color' : axiscolor},
-                'height' : 800,
-                'width' : 800,
-                'hovermode' : 'closest',
-                'uirevision' : '{}{}'.format(Xaxis, Yaxis),
-            }
+            "data": traces,
+            "layout": {
+                "xaxis": {"title": Xaxis, "color": axiscolor},
+                "yaxis": {"title": Yaxis, "color": axiscolor},
+                "height": 800,
+                "width": 800,
+                "hovermode": "closest",
+                "uirevision": "{}{}".format(Xaxis, Yaxis),
+            },
         }
+
     if run == True:
         app.run_server(debug=False)
     else:
-        return(app)
+        return app
--- genespector/layout.py
+++ genespector/layout.py
@@ -7,91 +7,130 @@ import numpy as np
 
 import sys
 
+
 def make_layout(graphdata, main_title, subtitle):
 
-    layout = html.Div([
-        html.H4(children=main_title,
-            style = {'textAlign': 'center'}),
-        html.H6(children= subtitle,
-            style = {'textAlign': 'center'}),
-        html.Div([
-            html.Div([
-                dcc.Graph(id='maingraph',
-                    config = {
-                        'toImageButtonOptions' : {
-                        'format' : 'png',
-                        'filename' : 'newplot',
-                        'height' : 1500,
-                        'width' : 1500,
-                        'scale' : 1.5
-                        }
-                    }
-                ),
-            ], className="two columns"),
-            html.Div([
-                html.Label(children='Split data by (categorical data):'),
-                dcc.Dropdown(
-                    id='split_dropdown',
-                    options=[{'label' : x, 'value' : x} for x in graphdata['catinfo'].columns.values],
-                    value=graphdata['catinfo'].columns.values[0]
+    layout = html.Div(
+        [
+            html.H4(children=main_title, style={"textAlign": "center"}),
+            html.H6(children=subtitle, style={"textAlign": "center"}),
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            dcc.Graph(
+                                id="maingraph",
+                                config={
+                                    "toImageButtonOptions": {
+                                        "format": "png",
+                                        "filename": "newplot",
+                                        "height": 1500,
+                                        "width": 1500,
+                                        "scale": 1.5,
+                                    }
+                                },
+                            ),
+                        ],
+                        className="two columns",
                     ),
-                html.Label(children='Colour points by (numeric data):'),
-                dcc.Dropdown(
-                    id='color_dropdown',
-                    options=[{'label' : x, 'value' : x} for x in np.concatenate((np.array(['categorical data']), graphdata['numkeys'], graphdata['genekeys']))],
-                    value='categorical data'
+                    html.Div(
+                        [
+                            html.Label(children="Split data by (categorical data):"),
+                            dcc.Dropdown(
+                                id="split_dropdown",
+                                options=[
+                                    {"label": x, "value": x}
+                                    for x in graphdata["catinfo"].columns.values
+                                ],
+                                value=graphdata["catinfo"].columns.values[0],
+                            ),
+                            html.Label(children="Colour points by (numeric data):"),
+                            dcc.Dropdown(
+                                id="color_dropdown",
+                                options=[
+                                    {"label": x, "value": x}
+                                    for x in np.concatenate(
+                                        (
+                                            np.array(["categorical data"]),
+                                            graphdata["numkeys"],
+                                            graphdata["genekeys"],
+                                        )
+                                    )
+                                ],
+                                value="categorical data",
+                            ),
+                            html.Label(children="Choose the X and Y axis of the plot"),
+                            dcc.Dropdown(
+                                id="Xaxis_choice",
+                                options=[
+                                    {"label": x, "value": x}
+                                    for x in graphdata["coords"].columns.values
+                                ],
+                                value=graphdata["coords"].columns.values[0],
+                            ),
+                            dcc.Dropdown(
+                                id="Yaxis_choice",
+                                options=[
+                                    {"label": x, "value": x}
+                                    for x in graphdata["coords"].columns.values
+                                ],
+                                value=graphdata["coords"].columns.values[1],
+                            ),
+                            html.Label(children="Axis"),
+                            dcc.RadioItems(
+                                id="Axis_switch",
+                                options=[
+                                    {"label": "ON", "value": "ON"},
+                                    {"label": "OFF", "value": "OFF"},
+                                ],
+                                value="ON",
+                            ),
+                            html.Label(children="Color scale: Min/Max"),
+                            dcc.Input(id="cmin_input", type="text", value=""),
+                            dcc.Input(id="cmax_input", type="text", value=""),
+                            html.Label(children="Choose point size"),
+                            dcc.Slider(
+                                id="pointsize_slider",
+                                min=2,
+                                max=14,
+                                value=7,
+                                marks={str(x): str(x) for x in range(2, 14, 2)},
+                            ),
+                            html.Br(),
+                            html.Label(children="Choose point opacity (%)"),
+                            dcc.Slider(
+                                id="opacity_slider",
+                                min=0,
+                                max=100,
+                                value=50,
+                                marks={x: str(x) for x in range(0, 110, 10)},
+                            ),
+                            html.Br(),
+                            html.Label(
+                                children="Genespector by: Iwo Kucinski, Gottgens lab",
+                                style={"textAlign": "left", "color": "#CCCCCC"},
+                            ),
+                            html.Label(
+                                children="Code: https://github.com/Iwo-K/genespector",
+                                style={"textAlign": "left", "color": "#CCCCCC"},
+                            ),
+                            html.Label(
+                                children="Based on Dash/Plotly framework (https://plot.ly/products/dash/)",
+                                style={"textAlign": "left", "color": "#CCCCCC"},
+                            ),
+                        ],
+                        style={
+                            "width": 400,
+                            "marginLeft": 200,
+                            "marginTop": 100,
+                            "float": "left",
+                        },
+                        className="two columns",
                     ),
-                    html.Label(children='Choose the X and Y axis of the plot'),
-                dcc.Dropdown(
-                    id='Xaxis_choice',
-                    options=[{'label' : x, 'value' : x} for x in graphdata['coords'].columns.values],
-                    value = graphdata['coords'].columns.values[0]
-                    ),
-                dcc.Dropdown(
-                    id='Yaxis_choice',
-                    options=[{'label' : x, 'value' : x} for x in graphdata['coords'].columns.values],
-                    value = graphdata['coords'].columns.values[1]
-                    ),
-                html.Label(children='Axis'),
-                dcc.RadioItems(
-                    id = 'Axis_switch',
-                    options=[
-                        {'label': 'ON', 'value': 'ON'},
-                        {'label': 'OFF', 'value': 'OFF'}],
-                    value='ON'),
-                html.Label(children='Color scale: Min/Max'),
-                dcc.Input(
-                    id = 'cmin_input',
-                    type = 'text',
-                    value=''),
-                dcc.Input(
-                    id = 'cmax_input',
-                    type = 'text',
-                    value=''),
-                html.Label(children='Choose point size'),
-                dcc.Slider(
-                    id='pointsize_slider',
-                    min=2,
-                    max=14,
-                    value=7,
-                    marks={str(x): str(x) for x in range(2, 14, 2)}),
-                html.Br(),
-                html.Label(children='Choose point opacity (%)'),
-                dcc.Slider(
-                    id='opacity_slider',
-                    min=0,
-                    max=100,
-                    value=50,
-                    marks={x : str(x) for x in range(0,110, 10)}),
-                html.Br(),
-                html.Label(children='Genespector by: Iwo Kucinski, Gottgens lab',
-                            style = {'textAlign': 'left', 'color' : '#CCCCCC'}),
-                html.Label(children='Code: https://github.com/Iwo-K/genespector',
-                            style = {'textAlign': 'left', 'color' : '#CCCCCC'}),
-                html.Label(children='Based on Dash/Plotly framework (https://plot.ly/products/dash/)',
-                            style = {'textAlign': 'left', 'color' : '#CCCCCC'})
-                ], style = {'width' : 400, 'marginLeft': 200, 'marginTop' : 100, 'float' : 'left'}, className="two columns")
-            ], className="row"),
-])
+                ],
+                className="row",
+            ),
+        ]
+    )
 
-    return(layout)
+    return layout
